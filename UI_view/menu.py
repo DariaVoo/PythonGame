f565aad811de6d9bb0fef2d6a5601ff792ab2113
Daria Vorontsova <dasha2003.voo@yandex.ru>
2020-06-22 21:50:14 +0300

    add try set image as background in menu

--- UI_view/menu.py
+++ UI_view/menu.py
@@ -5,11 +5,25 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 
 class MenuView(arcade.View):
+    def __init__(self):
+        super().__init__()
+        # Background image will be stored in this variable
+        self.background = None
+
+    def setup(self):
+        self.background = arcade.load_texture(":resources:images/backgrounds/abstract_1.jpg")
+        print("OK")
+
     def on_show(self):
         arcade.set_background_color(arcade.color.WHITE)
 
     def on_draw(self):
         arcade.start_render()
+
+        # Draw the background texture
+        # Выгрузка изображения на экран
+        # arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+
         arcade.draw_text("Menu Screen", SCREEN_WIDTH/2, SCREEN_HEIGHT/2,
                          arcade.color.BLACK, font_size=50, anchor_x="center")
         arcade.draw_text("Click to advance", SCREEN_WIDTH/2, SCREEN_HEIGHT/2-75,
